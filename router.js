dd380a125c97b25b4b08d868a7a8ebc53ee1dbf5
Francisco Ponce <francisco.eugp@gmail.com>
2020-10-01 23:53:02 -0500

    proved on http-server-spa functionalit

--- router.js
+++ router.js
@@ -1,30 +1,44 @@
-class Router {
+class Route {
   constructor(routes) {
     this.routes = routes
     this._loadInitialRoute()
   }
 
-  _loadRoute(...urlSegs) {
-    const matchedRoute = this._matchUrltoRoute(urlSegs)
+  loadRoute(...urlSegs) {
+    const matchedRoute = this._matchUrlToRoute(urlSegs)
+    // debugger
     const url = `/${urlSegs.join('/')}`
-    history.pushState({}, 'this works', url)
-
+    history.pushState({}, 'This works', url)
+    // debugger
     const routerOutElm = document.querySelectorAll('[data-router]')[0]
     routerOutElm.innerHTML = matchedRoute.template
   }
-  _matchUrltoRoute(urlSegs) {
+
+  _matchUrlToRoute(urlSegs) {
     const matchedRoute = this.routes.find((route) => {
-      const routePathSegs = route.path.split('/').slice(1)
-      if (routePathSegs.lenght !== urlSegs.length) return false
-      return routePathSegs.every((routePathSegs, i) => routePathSegs === urlSegs[i])
+      const routePathSegs = route.path.split('/').slice(0)
+      // debugger
+      if (routePathSegs.length !== urlSegs.length) {
+        // debugger
+        return false
+      }
+      // debugger
+      return routePathSegs.every((routePathSeg, i) => routePathSeg === urlSegs[i])
     })
+
+    if (matchedRoute === undefined) {
+      return {
+        template: '<h1>Not found</h1>',
+      }
+    }
+
     return matchedRoute
   }
 
   _loadInitialRoute() {
     const pathNameSplit = window.location.pathname.split('/')
-    const pathSegs = pathNameSplit.length > 1 ? pathNameSplit.splice(1) : ''
+    const pathSegs = pathNameSplit.length > 1 ? pathNameSplit.slice(1) : ''
 
-    this._loadRoute(...pathSegs)
+    this.loadRoute(...pathSegs)
   }
 }
--- routes.js
+++ routes.js
@@ -1,14 +1,18 @@
 const routes = [
   {
     path: '/',
-    template: '<h1>hola</h1>',
+    template: '<h1>Hola</h1>',
   },
   {
-    path: '/contacto',
-    template: '<h1>Contactos</h1>',
+    path: '/contact',
+    template: '<h1>Contacto</h1>',
   },
   {
     path: '/about',
-    template: '<h1>About Me</h1>',
+    template: '<h1>Acerca de mi</h1>',
+  },
+  {
+    path: '/index.html',
+    template: '<h1>Home</h1>',
   },
 ]
